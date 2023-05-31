bf81aef927e4a94dd38c04e78ef9385146770c52
chuWenShu <2040155715@qq.com>
2023-05-31 23:30:46 +0800

    文殊来了

--- src/index.jsx
+++ src/index.jsx
@@ -1,44 +1,27 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client"
-
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.setState = {}
+  }
 
-    constructor(props) {
-        super(props)
-
-        this.setState = {
-
-
-        }
-
-    }
-
-
-    render() {
-
-        return (
-            <div></div>
-
-        )
-
-    }
-
-
+  render() {
+    return <div></div>
+  }
 }
 
 export default function App() {
-
   return (
     <div>
-      <Index></Index> 
-      <h2>我是react</h2>
+      <Index></Index>
+      <h2>我是react 文殊到此一游</h2>
     </div>
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+console.log(document.getElementById('root'))
 root.render(<App />)
-
